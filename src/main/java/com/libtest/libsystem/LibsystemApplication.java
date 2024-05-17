c1e7ae368b49d1711f3468d5681ae45c8b20821c
monasr6 <mahamednasr6@gmail.com>
2024-05-17 17:57:51 +0300

    first commit

---
+++ src/main/java/com/libtest/libsystem/LibsystemApplication.java
@@ -0,0 +1,13 @@
+package com.libtest.libsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/libtest/libsystem/LibsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libtest.libsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
