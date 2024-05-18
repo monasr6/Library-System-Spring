e35c79a8ee61155fd84f7cf5d20fe259e5fc1ccf
monasr6 <mahamednasr6@gmail.com>
2024-05-18 15:14:46 +0300

    add validation token and edit get claims methods

--- src/main/java/com/libtest/libsystem/config/JwtService.java
+++ src/main/java/com/libtest/libsystem/config/JwtService.java
@@ -1,10 +1,16 @@
 package com.libtest.libsystem.config;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 import javax.crypto.SecretKey;
 
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+
+import com.libtest.libsystem.user.User;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
@@ -16,28 +22,37 @@ import io.jsonwebtoken.security.Keys;
 public class JwtService {
   SecretKey key = Keys.hmacShaKeyFor(Decoders.BASE64.decode("secret"));
 
-  public String createToken(String id, String name) {
+  public String createToken(User user) {
+    return createToken(new HashMap<>(), user);
+  }
+
+  public String createToken(Map<String, Object> claims, UserDetails userDetails) {
     return Jwts.builder()
-        .setHeaderParam("typ", "JWT")
-        .setHeaderParam("regDate", System.currentTimeMillis())
-        .setSubject("user")
-        .claim("id", id)
-        .claim("name", name)
+        .setIssuer("libsystem")
+        .setIssuedAt(new java.util.Date())
+        .setExpiration(new java.util.Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
+        .setSubject(userDetails.getUsername())
+        .setClaims(claims)
         .signWith(key, SignatureAlgorithm.HS256)
         .compact();
-
   };
+
+  public boolean validateToken(String token, UserDetails userDetails) {
+    final String username = getEmail(token);
+    return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+  }
+
+  private boolean isTokenExpired(String token) {
+    Date expiration = extractClaim(token, Claims::getExpiration);
+    return expiration.before(new Date());
+  }
 
   public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
     final Claims claims = getAllClaims(token);
     return claimsResolver.apply(claims);
   }
 
-  public String getId(String token) {
-    return extractClaim(token, Claims::getId);
-  }
-
-  public String getName(String token) {
+  public String getEmail(String token) {
     return extractClaim(token, Claims::getSubject);
   }
 
